ec58b6dc03e1a78c751836a5bcdb6a75e5753964
LightningWB <lightningwb@gmail.com>
2021-08-28 14:33:58 -0500

    delete owner from original reality anchor when a new one is placed

--- src/equipment/reality_anchor.js
+++ src/equipment/reality_anchor.js
@@ -1,8 +1,24 @@
 const {emit, players, util, chunks, options, generateTileAt} = require('../bullet');
 
+const removeRespawn = (loc) => {
+	if(!chunks.isChunkLoaded(loc.x, loc.y)) {
+		chunks.loadChunk(loc.x, loc.y);
+	}
+	const interval = setInterval(() => {
+		if(chunks.isChunkLoaded(loc.x, loc.y)) {
+			clearInterval(interval);
+			delete chunks.getObject(loc.x, loc.y).private.owner;
+		}
+	}, 10);
+}
+
 module.exports.placed = function(reality_anchor, player) {
   reality_anchor.private.owner = player.public.username;
+  if(player.private.respawnPoint) {
+	  removeRespawn(player.private.respawnPoint);
+  }
   player.private.respawnPoint = { x: reality_anchor.public.x, y: reality_anchor.public.y };
+  emit('travelers', 'eventLog', 'successfully anchored life force.', player);
 }
 
 module.exports.broke = function(reality_anchor, _player) {
